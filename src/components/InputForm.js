223c0d7d2af07104cd3bbf143284e780ed3cf217
holgermueller <9jholger@gmail.com>
2022-10-13 21:40:29 -0500

    form works some styling

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
+
 export default function InputForm() {
+  const [formData, setFormData] = useState({
+    todo: "",
+  });
+
+  const { todo } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(todo);
+  };
+
   return (
     <section>
-      <form className="form">
-        <input className="input" type="text" placeholder="To do..." />
-        <input className="button" type="button" value="Submit" />
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="input"
+          name="todo"
+          type="text"
+          placeholder="To do..."
+          value={todo}
+          onChange={onChange}
+        />
+        <input className="button" type="submit" value="Submit" />
       </form>
     </section>
   );
