88628bcce96bfc37d28cb446c45f2fed9182a6d1
holgermueller <9jholger@gmail.com>
2022-10-16 16:32:01 -0500

    some styling

--- src/components/ToDos.js
+++ src/components/ToDos.js
@@ -1,27 +1,21 @@
 import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todos/todoSlice";
 
 export default function ToDos() {
   const { todos } = useSelector((state) => state.todos);
   const dispatch = useDispatch();
-
-  const removeTodo = (e) => {
-    console.log(e.target.id);
-    const todoToRemove = e.target.id;
-
-    dispatch(removeTodo(todoToRemove));
-  };
 
   return (
     <section className="to-do-list">
       <div className="todo-grid">
         {todos.map((todo, i) => (
           <div className="todo-item" key={i}>
-            {todo.name}
+            <div className="todo-name">{todo.name}</div>
             <div className="done-button-container">
               <button
-                id={todo.name}
+                id={todo.id}
                 className="done-button"
-                onClick={removeTodo}
+                onClick={() => dispatch(removeTodo(todo.id))}
               >
                 DONE
               </button>
--- src/features/todos/todoSlice.js
+++ src/features/todos/todoSlice.js
@@ -1,13 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 export const todoSlice = createSlice({
   name: "todos",
   initialState: {
     todos: [
-      { name: "buy milk" },
-      { name: "mow lawn" },
-      { name: "escape" },
-      { name: "make dinner" },
+      { name: "buy milk", id: "1" },
+      { name: "mow lawn", id: "2" },
+      { name: "escape", id: "3" },
+      { name: "make dinner", id: "4" },
+      { name: "No, data is not persisted with a backend", id: "5" },
     ],
   },
   reducers: {
@@ -18,9 +19,17 @@ export const todoSlice = createSlice({
     },
     removeTodo(state, action) {
       console.log(action.payload);
+
+      current(state.todos).map((item) => console.log(item.name));
+
+      // let result = current(state.todos).indexOf(
+      //   (item) => item.id === action.payload
+      // );
+      // console.log(result);
+      // console.log(todoToRemove);
     },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, removeTodo } = todoSlice.actions;
 export default todoSlice.reducer;
